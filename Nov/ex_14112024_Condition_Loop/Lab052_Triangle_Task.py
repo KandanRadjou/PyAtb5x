e0b85e87661b4b74b1f01d9e1148df52dee30e87
kandan <kandan.radjou@gmail.com>
2024-11-15 23:24:57 +0530

    Updated 14Nov

--- Nov/ex_14112024_Condition_Loop/Lab052_Triangle_Task.py
+++ Nov/ex_14112024_Condition_Loop/Lab052_Triangle_Task.py
@@ -1,1 +1,9 @@
-s1=int(input("enter the side 1\n"))
+s1=float(input("enter the side s1\n"))
+s2=float(input("enter the side s2\n"))
+s3=float(input("enter the side s3\n"))
+if s1==s2==s3:
+    print("It is a equilateral triangle")
+elif s1==s2:
+    print("It is a isosceles")
+else:
+    print("It is a Scalene")
